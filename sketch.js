af911c4643be8d53b2c5fb3344daf961010ffb2d
CodeGod10 <vijidnp@gmail.com>
2025-11-22 11:53:15 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function codeButton(){
+    window.location.href = "coding.html"
+}
+
+function artButton(){
+    window.location.href = "art.html"
+}
+function home(){
+    window.location.href = "index.html"
+}
+function b1(){
+    window.open ("https://code.org/en-US")
+}
+function b2(){
+    window.open ("https://dialogflow.cloud.google.com/")
+}
+function b3(){
+    window.open ("https://appinventor.mit.edu/")
+}
+function b4(){
+    window.open ("https://codepen.io/")
+}
+function b5(){
+    window.open("https://code.visualstudio.com/")
+}
+function b6(){
+    window.open ("https://github.com/")
+}
+
